e7bc81918aa603a45ea036b0c067b0b409a3af27
Mohammad Rastegar <mohammad.rastegarmolavi@gmail.com>
2020-04-02 00:53:35 +0430

    3 commit

--- hamahang/models.py
+++ hamahang/models.py
@@ -1,3 +1,79 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class mdl_sale(models.Model):
+    sale_choices=(
+        ("apartement","apartement"),
+        ("land","land"),
+        ("home","home"),
+    )
+    sale =models.CharField(choices=sale_choices,default="apartement", max_length=300)
+
+    def __str__(self):
+        return self.sale
+
+class mdl_apartement(models.Model):
+    
+    apartement = models.ForeignKey(mdl_sale, on_delete=models.CASCADE)
+    img =models.ImageField()
+    topic =models.CharField( max_length=300)
+    text =models.TextField(max_length=1000)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.topic
+
+
+class mdl_land(models.Model):
+    
+    land = models.ForeignKey(mdl_sale, on_delete=models.CASCADE)
+    img =models.ImageField()
+    topic =models.CharField( max_length=300)
+    text =models.TextField(max_length=1000)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.topic
+
+    
+
+class mdl_home(models.Model):
+    
+    home = models.ForeignKey(mdl_sale, on_delete=models.CASCADE)
+    img =models.ImageField()
+    topic =models.CharField( max_length=300)
+    text =models.TextField(max_length=1000)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.topic
+
+   
+class mdl_rent(models.Model):
+    
+    img = models.FilePathField(path="/images")
+    topic =models.CharField( max_length=300)
+    text =models.TextField(max_length=1000)
+    date = models.DateTimeField(null=True)
+
+
+
+    def __str__(self):
+            return self.topic
+ 
+    
+
+
+
+class mdl_participation(models.Model):
+    
+    img =models.ImageField()
+    topic =models.CharField( max_length=300)
+    text =models.TextField(max_length=1000)
+    date = models.DateTimeField()
+
+
+
+    def __str__(self):
+            return self.topic
--- hamahang/views.py
+++ hamahang/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from . import  models
+def index(request):
+    sales =models.mdl_sale.objects.all()
+    apartements=models.mdl_apartement.objects.all()
+    lands =models.mdl_land.objects.all()
+    homes =models.mdl_home.objects.all()
+    rents =models.mdl_rent.objects.all()
 
-# Create your views here.
+    return render(request,
+    "index.html",
+    context={'sales':sales,'apartements':apartements,'lands':lands,'homes':homes,'rents':rents}
+    )
+
+
+def rents(request):
+    pass
+
+
+
+
+ 
