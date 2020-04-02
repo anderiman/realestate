4e70d184f5273c9e885f4db29b5a38940165c641
Mohammad Rastegar <mohammad.rastegarmolavi@gmail.com>
2020-04-02 05:15:29 +0430

    4 commit

--- hamahang/views.py
+++ hamahang/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from . import  models
 def index(request):
     sales =models.mdl_sale.objects.all()
-    apartements=models.mdl_apartement.objects.all()
-    lands =models.mdl_land.objects.all()
-    homes =models.mdl_home.objects.all()
-    rents =models.mdl_rent.objects.all()
+    # apartements=models.mdl_apartement.objects.all()
+    # lands =models.mdl_land.objects.all()
+    # homes =models.mdl_home.objects.all()
+    # rents =models.mdl_rent.objects.all()
 
     return render(request,
     "index.html",
-    context={'sales':sales,'apartements':apartements,'lands':lands,'homes':homes,'rents':rents}
+    context={'sales':sales}
     )
 
 
